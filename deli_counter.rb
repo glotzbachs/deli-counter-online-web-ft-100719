02ea9ffec9c488ac1a84982be0b36d1453244c15
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-08 19:41:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(array)
   if array.length==0
     puts "The line is currently empty."
   else 
-    puts "The line is currently: "
     array.each_with_index do |name,index|
+      puts "The line is currently: "
       puts"#{index}. #{name}"
     end
   end
