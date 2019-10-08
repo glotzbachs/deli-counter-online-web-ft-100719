1ace34290a40e69009863a12950ab9b6bea9c5e0
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-08 19:50:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
     list=[]
     array.each_with_index do |name,index|
       list<< "#{index+1}. #{name}"
-      puts "The line is currently: "+ list.join(" ")
     end
+    puts "The line is currently: "+ list.join(" ")
   end
 end
